5f9909ed305a3b34ec15a13bc7b4e7592418b550
hhimanshu <harit.himanshu@yahoo.in>
2017-01-28 17:19:54 -0800

    - get todoItem from Server, create 1 todoItem on client and render them both. This shows how common ToDoItem is both on client and server

--- client/src/main/scala/example/Client.scala
+++ client/src/main/scala/example/Client.scala
@@ -1,6 +1,8 @@
 package example
 
 import org.scalajs.dom
+import org.scalajs.dom.ext.Ajax
+import shared.SharedMessages.ToDoItem
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExport
@@ -11,14 +13,29 @@ import scalatags.JsDom.all._
 object Client extends js.JSApp {
   @JSExport
   def main(): Unit = {
-    val host = sys.env.getOrElse("APP_URL", "localhost")
-    println(s"env variables: ${sys.env}")
+    import scala.scalajs.concurrent.JSExecutionContext.Implicits.runNow
+
     dom.document.body.appendChild(
       div(
         h1("Hello World!"),
-        p("I am happy to be running on Full Stack Scala"),
-        p (s"I am running on $host")
+        p("I am happy to be running on Full Stack Scala")
+      ).render
+    )
 
+    Ajax.get("/todo").onSuccess { case xhr =>
+      dom.document.body.appendChild(
+        div(
+          h4("Server Message"),
+          p (xhr.responseText)
+        ).render
+      )
+    }
+
+    val todoItem = ToDoItem("straight from client")
+    dom.document.body.appendChild(
+      div(
+        h4("Client Message"),
+        p (todoItem.desc)
       ).render
     )
   }
